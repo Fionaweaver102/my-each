fa492b9c76020d1ab51c5baff3e84eadaf41c84e
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-11 10:40:36 -0700

    commit tree 997bc2d6ae6774fc0ac594265fcb674425ba645a

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+  x = 0
+  while array.length > x 
+    yield(array[x])
+    x+=1
+  end 
+  array 
+else 
+   "This block should not run!"
+end 
 end
+
+my_each([1,2,3,4]) do |num|
+   num
+end 
